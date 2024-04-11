a1ace96aa33e48c62ca405d2201eb8ece24e5fb4
isaias-alt <cascolucasisaias@gmail.com>
2024-04-11 14:41:27 -0400

    add users controllers

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,8 +1,72 @@
-const getUsers = async (_, res) => { }
+const { User } = require("../models/user.model");
 
-const getUserById = async (req, res) => { }
+const getUsers = async (_, res) => {
+  try {
+    const users = await User.find().select('name email id isAdmin');
+    if (!users) {
+      return res.status(404).json({
+        message: 'Users not found',
+        code: 404
+      });
+    }
+    return res.json(users);
 
-const updateUser = async (req, res) => { }
+  } catch (error) {
+    return res.status(500).json({
+      type: error.name,
+      message: error.message,
+      code: 500,
+    });
+  }
+}
+
+const getUserById = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id).select('-passwordHash -resetPasswordOtp -resetPasswordOtpExpired');
+    if (!user) {
+      return res.status(404).json({
+        message: 'User not found',
+        code: 404
+      });
+    }
+    return res.json(user);
+
+  } catch (error) {
+    return res.status(500).json({
+      type: error.name,
+      message: error.message,
+      code: 500,
+    });
+  }
+}
+
+const updateUser = async (req, res) => {
+  try {
+    const { name, email, phone } = req.body;
+    const user = await User.findByIdAndUpdate(
+      req.params.id,
+      { name, email, phone },
+      { new: true },
+    );
+
+    if (!user) {
+      return res.status(404).json({
+        message: 'User not found',
+        code: 404
+      });
+    }
+
+    user.passwordHash = undefined;
+    return res.json(user);
+
+  } catch (error) {
+    return res.status(500).json({
+      type: error.name,
+      message: error.message,
+      code: 500,
+    });
+  }
+}
 
 
 module.exports = { getUsers, getUserById, updateUser };
