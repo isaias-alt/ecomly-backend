a92d29f48f8d0a27665202584062659e503648cc
isaias-alt <cascolucasisaias@gmail.com>
2024-04-11 15:40:21 -0400

    delete spaces

--- routes/auth.router.js
+++ routes/auth.router.js
@@ -5,15 +5,10 @@ const authController = require('../controllers/auth.controller');
 const { validateUser, validatePassword } = require('../validators/auth.validator')
 
 router.post('/login', authController.login);
-
 router.post('/register', validateUser, authController.register);
-
 router.post('/forgot-password', authController.forgotPassword);
-
 router.post('/verify-otp', authController.verifyPasswordResetOTP);
-
 router.post('/reset-password', validatePassword, authController.resetPassword);
-
 router.get('/verify-token', authController.verifyToken);
 
 module.exports = router;
--- routes/users.router.js
+++ routes/users.router.js
@@ -4,9 +4,7 @@ const router = express.Router();
 const usersController = require('../controllers/users.controller');
 
 router.get('/', usersController.getUsers);
-
 router.get('/:id', usersController.getUserById);
-
 router.put('/:id', usersController.updateUser);
 
 module.exports = router;
