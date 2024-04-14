7d4060e3bf925c2bd84f70546e73ed9ee0291fff
isaias-alt <cascolucasisaias@gmail.com>
2024-04-14 17:18:37 -0400

    add category model

---
+++ models/category.model.js
@@ -0,0 +1,10 @@
+const { Schema, model } = require("mongoose");
+
+const categorySchema = Schema({
+  name: { type: String, required: true },
+  colour: { type: String, default: '#000000' },
+  image: { type: String, required: true },
+  markedForDeletion: { type: Boolean, default: false }
+});
+
+exports.Category = model('Category', categorySchema);
