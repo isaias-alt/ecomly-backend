b5e4102672856f9172495952bcf30e6f860a6b2f
isaias-alt <cascolucasisaias@gmail.com>
2024-04-14 17:18:55 -0400

    add order model

---
+++ models/order.model.js
@@ -0,0 +1,45 @@
+const { Schema, model } = require("mongoose");
+
+const orderSchema = Schema({
+  shippingAddress: { type: String, required: true },
+  city: { type: String, required: true },
+  postalCode: String,
+  country: { type: String, required: true },
+  phone: { type: String, required: true },
+  paymentId: String,
+  status: {
+    type: String,
+    required: true,
+    default: 'pending',
+    enum: [
+      'pending',
+      'processed',
+      'shipped',
+      'out-for-delivery',
+      'delivered',
+      'cancelled',
+      'on-hold',
+      'expired',
+    ],
+  },
+  statusHistory: {
+    type: [String],
+    required: true,
+    default: ['pending'],
+    enum: [
+      'pending',
+      'processed',
+      'shipped',
+      'out-for-delivery',
+      'delivered',
+      'cancelled',
+      'on-hold',
+      'expired',
+    ],
+  },
+  totalPrice: Number,
+  user: { type: Schema.Types.ObjectId, ref: 'User' },
+  dateOrdered: { type: Date, default: Date.now }
+});
+
+exports.Order = model('Order', orderSchema);
