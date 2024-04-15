2d304f40f625c416c36755a23e29165e5839ff36
isaias-alt <cascolucasisaias@gmail.com>
2024-04-15 17:29:09 -0400

    add review model

--- models/review.model.js
+++ models/review.model.js
@@ -1,1 +1,14 @@
-// TODO: review model 
+const { Schema, model } = require('mongoose');
+
+const reviewSchema = Schema({
+  user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+  userName: { type: String, required: true },
+  comment: { type: String, trim: true },
+  rating: { type: Number, required: true },
+  date: { type: Date, default: Date.now }
+});
+
+cartProductoSchema.set('toObject', { virtuals: true });
+cartProductoSchema.set('toJSON', { virtuals: true });
+
+exports.Review = model('Review', reviewSchema);
