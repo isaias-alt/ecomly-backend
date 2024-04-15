f9e5e42bb6e5d5c10eb86361dd373229bbf84105
isaias-alt <cascolucasisaias@gmail.com>
2024-04-15 17:31:59 -0400

    fix schema name

--- models/cart_product.model.js
+++ models/cart_product.model.js
@@ -1,6 +1,6 @@
 const { Schema, model } = require('mongoose');
 
-const cartProductoSchema = Schema({
+const cartProductSchema = Schema({
   product: { type: Schema.Types.ObjectId, ref: 'Product', required: true },
   productName: { type: String, required: true },
   productImage: { type: String, required: true },
@@ -15,7 +15,7 @@ const cartProductoSchema = Schema({
   reserved: { type: Boolean, default: true },
 });
 
-cartProductoSchema.set('toObject', { virtuals: true });
-cartProductoSchema.set('toJSON', { virtuals: true });
+cartProductSchema.set('toObject', { virtuals: true });
+cartProductSchema.set('toJSON', { virtuals: true });
 
-exports.CartProducto = model('CartProducto', cartProductoSchema);
+exports.CartProduct = model('CartProduct', cartProductSchema);
