14abf6f2b93f797ecdb2d344e9a5ef9da455e94b
isaias-alt <cascolucasisaias@gmail.com>
2024-04-18 12:22:26 -0400

    finish addCategory controllers

--- controllers/admin/categories.controller.js
+++ controllers/admin/categories.controller.js
@@ -1,5 +1,48 @@
+const util = require('util');
+
+const mediaHelper = require('../../helpers/media.helper');
+const { Category } = require('../../models/category.model');
+
 const addCategory = async (req, res) => {
   try {
+    const uploadImage = util.promisify(
+      mediaHelper.upload.fields([
+        { name: 'image', maxCount: 1 }
+      ])
+    );
+
+    try {
+      await uploadImage(req, res);
+
+    } catch (error) {
+      return res.status(500).json({
+        type: error.name,
+        message: `${error.message}{${error.fields}}`,
+        storageErrors: error.storageErrors,
+        code: 500
+      });
+    }
+
+    const image = req.files['image'][0];
+    if (!image) {
+      return res.status(404).json({
+        message: 'Not image found!',
+        code: 404,
+      });
+    }
+
+    req.body['image'] = `${req.protocol}://${req.get('host')}/${image.path}`;
+    let category = new Category(req.body);
+
+    category = category.save();
+
+    if (!category) {
+      return res.status.json({
+        message: 'The category could not be created'
+      });
+    }
+
+    return res.status(201).json({ category });
 
   } catch (error) {
     return res.status(500).json({
